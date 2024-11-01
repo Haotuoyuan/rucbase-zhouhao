ba3ae96ce2e78ce909ee74af974853bc2486a02f
Haotuoyuan <zhouhao183@ruc.edu.cn>
2024-11-01 09:54:19 -0700

    Initial commit

--- src/transaction/transaction_manager.cpp
+++ src/transaction/transaction_manager.cpp
@@ -9,10 +9,11 @@ MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
 See the Mulan PSL v2 for more details. */
 
 #include "transaction_manager.h"
+#include <mutex>
 #include "record/rm_file_handle.h"
 #include "system/sm_manager.h"
 
-std::unordered_map<txn_id_t, Transaction *> TransactionManager::txn_map = {};
+std::unordered_map<txn_id_t, Transaction*> TransactionManager::txn_map = {};
 
 /**
  * @description: 事务的开始方法
@@ -20,13 +21,13 @@ std::unordered_map<txn_id_t, Transaction *> TransactionManager::txn_map = {};
  * @param {Transaction*} txn 事务指针，空指针代表需要创建新事务，否则开始已有事务
  * @param {LogManager*} log_manager 日志管理器指针
  */
-Transaction * TransactionManager::begin(Transaction* txn, LogManager* log_manager) {
+Transaction* TransactionManager::begin(Transaction* txn, LogManager* log_manager) {
     // Todo:
     // 0. 给txn_map_上锁
-    std::scoped_lock lock{latch_}; 
+    std::scoped_lock lock{latch_};
     // 1. 判断传入事务参数是否为空指针
     // 2. 如果为空指针，创建新事务
-    if(!txn){
+    if (!txn) {
         txn = new Transaction(next_txn_id_);
         next_txn_id_++;
         // TODO
@@ -47,21 +48,21 @@ Transaction * TransactionManager::begin(Transaction* txn, LogManager* log_manage
 void TransactionManager::commit(Transaction* txn, LogManager* log_manager) {
     // Todo:
     // 0. 给txn_map_上锁
-    std::scoped_lock lock{latch_}; 
-    if(!txn){
+    std::scoped_lock lock{latch_};
+    if (!txn) {
         return;
     }
     // 1. 如果存在未提交的写操作，提交所有的写操作
     auto write_set = txn->get_write_set();
-    while(!write_set->empty()){
+    while (!write_set->empty()) {
         // TODO
         write_set->pop_front();
     }
     // 2. 释放所有锁
     auto lock_set = txn->get_lock_set();
-    if(!lock_set->empty()){
-        for(auto it=lock_set->begin();it!=lock_set->end();it++){
-            lock_manager_->unlock(txn,*it);
+    if (!lock_set->empty()) {
+        for (auto it = lock_set->begin(); it != lock_set->end(); it++) {
+            lock_manager_->unlock(txn, *it);
         }
     }
     lock_set->clear();
@@ -78,12 +79,45 @@ void TransactionManager::commit(Transaction* txn, LogManager* log_manager) {
  * @param {Transaction *} txn 需要回滚的事务
  * @param {LogManager} *log_manager 日志管理器指针
  */
-void TransactionManager::abort(Transaction * txn, LogManager *log_manager) {
+void TransactionManager::abort(Transaction* txn, LogManager* log_manager) {
     // TODO 1.:
     // 0. 给txn_map_上锁
+    std::scoped_lock lock{latch_};
+    if (!txn) {
+        return;
+    }
     // 1. 回滚所有写操作
+    auto write_set = txn->get_write_set();
+    auto* context = new Context(lock_manager_, log_manager, txn);
+    for (auto iter = write_set->rbegin(); iter != write_set->rend(); ++iter) {
+        auto& type = (*iter)->GetWriteType();
+        auto& rid = (*iter)->GetRid();
+        auto buf = (*iter)->GetRecord().data;
+        auto fh = sm_manager_->fhs_.at((*iter)->GetTableName()).get();
+        switch (type) {
+            case WType::INSERT_TUPLE:
+                fh->delete_record(rid, context);
+                break;
+            case WType::DELETE_TUPLE:
+                fh->insert_record(buf, context);
+                break;
+            case WType::UPDATE_TUPLE:
+                fh->update_record(rid, buf, context);
+                break;
+        }
+    }
+    write_set->clear();
     // 2. 释放所有锁
+    auto lock_set = txn->get_lock_set();
+    if (!lock_set->empty()) {
+        for (auto it = lock_set->begin(); it != lock_set->end(); it++) {
+            lock_manager_->unlock(txn, *it);
+        }
+    }
+    lock_set->clear();
     // 3. 清空事务相关资源，eg.锁集
     // 4. 把事务日志刷入磁盘中
+    log_manager->flush_log_to_disk();
     // 5. 更新事务状态
+    txn->set_state(TransactionState::ABORTED);
 }
